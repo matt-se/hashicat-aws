bdf2e0fb304552d6255439895f5cadb2f9310b4e
matt-se <matt.greco@hashicorp.com>
2022-11-14 19:26:04 +0000

    Added the second tag

--- main.tf
+++ main.tf
@@ -128,7 +128,8 @@ resource "aws_instance" "hashicat" {
 
   tags = {
     Name = "${var.prefix}-hashicat-instance"
-    Department = "devops"
+    Department = "devopss"
+ #   Billable = "true"
   }
 }
 
